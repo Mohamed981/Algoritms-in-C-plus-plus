de08906dd6d9ae86b670c591691ecb509134cd73
Mohamed981 <48105978+Mohamed981@users.noreply.github.com>
2021-08-11 14:20:25 +0200

    Update Box Stacking.cpp
    You are given a set of n types of rectangular 3-D boxes, where the i^th box has height h(i), width w(i) and depth d(i) (all real numbers). You want to create a stack of boxes which is as tall as possible, but you can only stack a box on top of another box if the dimensions of the 2-D base of the lower box are each strictly larger than those of the 2-D base of the higher box
    
    STEPS
    1.Visualize the Problem
    2.Find an appropriate subproblem
    3.Find relationships among subproblems
    4.Generalize the relashionship
    5.Implement by solving subproblems in order

--- Box Stacking.cpp
+++ Box Stacking.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-/*
-Stacks 
-*/
+
 bool sortcol( const vector<int>& v1,
                const vector<int>& v2 ) {
  return v1[1] < v2[1];
 }
+
 bool canbestacked(vector<int>& top,vector<int>& bottom){
     if (top[0]<bottom[0] && top[1]<bottom[1])
         return 1;
@@ -16,9 +15,12 @@ bool canbestacked(vector<int>& top,vector<int>& bottom){
 }
 int main()
 {
+    //Boxes
     vector<vector<int>> vect{{1,5,4},{1,2,2},{2,3,2},{2,4,1},{3,6,2},{4,5,3}};
+    //Sorting according to length
     sort(vect.begin(), vect.end(),sortcol);
     vector<int> heights;
+    //Storing heights of each box
     for (int i=0; i<vect.size(); i++)
     {
         heights.push_back(vect[i][2]);
@@ -28,6 +30,7 @@ int main()
     for(int i=1;i<vect.size();i++){
 
         for (int j=0;j<i;j++){
+            //Check if previous boxes can be stacked
             if(canbestacked(vect[j],vect[i]))
                 if(maximum<heights[j])
                     maximum=heights[j];
